63f42570c3c4b3b52f3afd4a2b635f0c924091d2
Nand <studynand@18gmail.com>
2025-07-05 23:41:25 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/React-Router-Project/",
+  plugins: [react()] 
+ 
 })
